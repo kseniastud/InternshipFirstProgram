f0666bcb0c0fffd861353311bf110eaa2cc003c7
Ksenia <oksanastudent3@gmail.com>
2019-07-18 09:45:49 +0300

    Added the BeforeTest

--- src/test/java/SiteUrlTest.java
+++ src/test/java/SiteUrlTest.java
@@ -1,4 +1,25 @@
-package PACKAGE_NAME;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
 
 public class SiteUrlTest {
+    private WebDriver driver;
+    @BeforeTest
+    public void createChromeDriver(){
+        System.setProperty("webdriver.chrome.driver", "\\automation\\driver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        System.out.println("@BeforeTest");
+    }
+
+    @Test
+    public void enterToTheSiteAndAssertion(){
+
+    }
+
+    @AfterTest
+    public void closeAllTabs(){
+
+    }
 }
