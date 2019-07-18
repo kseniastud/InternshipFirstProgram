5e1cc6f726b81f3942de3a86ac18d1df6e153b63
Ksenia <oksanastudent3@gmail.com>
2019-07-18 09:49:25 +0300

    Added the Test

--- src/test/java/SiteUrlTest.java
+++ src/test/java/SiteUrlTest.java
@@ -1,8 +1,13 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.ArrayList;
+import java.util.concurrent.TimeUnit;
 
 public class SiteUrlTest {
     private WebDriver driver;
@@ -15,7 +20,22 @@ public class SiteUrlTest {
 
     @Test
     public void enterToTheSiteAndAssertion(){
-
+        driver.get("https://www.google.ru/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        WebElement element = driver.findElement(By.xpath("//input[@name='q']"));
+        element.sendKeys("nnm-club");
+        element.submit();
+        driver.manage().timeouts().pageLoadTimeout(10, TimeUnit.SECONDS);
+        WebElement element1= driver.findElement(By
+                .xpath("//*[contains(text(),'NNM-Club: Торрент-трекер')]"));
+        element1.click();
+        ArrayList<String> tabs2 = new ArrayList<String> (driver.getWindowHandles());
+        driver.switchTo().window(tabs2.get(1));
+        String url1 = new String("http://nnmclub.to/");
+        driver.manage().timeouts().pageLoadTimeout(100, TimeUnit.SECONDS);
+        String url2 = driver.getCurrentUrl();
+        System.out.println("@Test");
     }
 
     @AfterTest
